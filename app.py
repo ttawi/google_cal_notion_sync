9b63efa092ac75eca5db5e0f30ccf4145d4a827e
Qian Yan <qian.will.yan@gmail.com>
2021-10-29 19:37:43 +0800

    add google API and notion API call

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from __future__ import print_function
 import datetime
+import logging
 import os.path
+from typing import Any
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+import httpx
+from notion_client import Client
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
-APP_CLIENT_CREDENTIAL_PATH = 'secret/credentials.json'
-USER_TOKEN_PAHT = 'secret/token.json'
+# secret path
+APP_CLIENT_CREDENTIAL_PATH = "secret/credentials.json"
+USER_TOKEN_PAHT = "secret/token.json"
+NOTION_INTEGRATION_CREDENTIAL_PATH = "secret/notion-secret.txt"
 
-def read_cred() -> Credentials:
+# notion
+NOTION_CALENDAR_DB_ID = "56de8fb5-bae9-47b2-80bc-7c79db8b5cba"
+
+
+def _get_google_credential() -> Credentials:
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -36,27 +46,22 @@ def read_cred() -> Credentials:
     return creds
 
 
-def read_calendar() -> None:
+def _read_calendar(start_time: str, end_time: str) -> None:
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    creds = read_cred()
+    creds = _get_google_credential()
     service = build("calendar", "v3", credentials=creds)
 
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    two_yr_later = (
-        datetime.datetime.utcnow() + datetime.timedelta(days=365 * 2)
-    ).isoformat() + "Z"  # 'Z' indicates UTC time
-    print(two_yr_later)
 
     print("Getting the upcoming events")
     events_result = (
         service.events()
         .list(
             calendarId="primary",
-            timeMin=now,
-            timeMax=two_yr_later,
+            timeMin=start_time,
+            timeMax=end_time,
             singleEvents=True,
             orderBy="startTime",
         )
@@ -72,5 +77,52 @@ def read_calendar() -> None:
         print(start, end, event["summary"])
 
 
+def _read_notion(start_time: str, end_time: str) -> None:
+    credential = open(NOTION_INTEGRATION_CREDENTIAL_PATH, "r").readline()
+    notion = Client(
+        auth=credential,
+    )
+
+    date_filter = {
+        "and": [
+            {"property": "Date", "date": {"on_or_after": start_time}},
+            {"property": "Date", "date": {"before": end_time}},
+        ]
+    }
+    notion_events = notion.databases.query(NOTION_CALENDAR_DB_ID, filter=date_filter)
+    print(notion_events)
+
+
+# An util function to give each page a default date equals to creation date if they do not have one
+def _notion_set_date(start_cursor: str = None):
+    credential = open(NOTION_INTEGRATION_CREDENTIAL_PATH, "r").readline()
+    notion = Client(
+        auth=credential,
+    )
+
+    # we can use filter but I want to play w/ cursor.
+    if start_cursor:
+        response = notion.databases.query(
+            NOTION_CALENDAR_DB_ID, start_cursor=start_cursor
+        )
+    else:
+        response = notion.databases.query(NOTION_CALENDAR_DB_ID)
+    pages = response["results"]
+    pages = list(filter(lambda page: not page["properties"]["Date"]["date"], pages))
+
+    for page in pages:
+        notion.pages.update(
+            page["id"], properties={"Date": {"date": {'start': page["created_time"], 'end': None}}}
+        )
+    if response["has_more"]:
+        _notion_set_date(response["next_cursor"])
+
+def main() -> None:
+    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    two_yr_later = (
+        datetime.datetime.utcnow() + datetime.timedelta(days=365 * 2)
+    ).isoformat() + "Z"  # 'Z' indicates UTC time
+    read_notion(now, two_yr_later)
+
 if __name__ == "__main__":
-    read_calendar()
+    main()
