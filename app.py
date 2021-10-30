3fea5fd091513db243a6a0d493950eb8e46aa996
qiany <qian.will.yan@gmail.com>
2021-10-30 16:03:05 +0800

    minor

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-import httpx
 from notion_client import Client
 
 from event import Event
